c12e68839a5d58a82ab746f5c6f9b9fb82981df8
Anshuman K <tooringtest@gmail.com>
2017-04-10 10:17:29 +0530

    fix(scripts): Added environment variable to remove performance degradation warning

--- trac-admin.sh
+++ trac-admin.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-docker run -v `pwd`/container/trac:/trac -v `pwd`/container/repos:/repos tooringanalytics/trac-server trac-admin /trac $@
+docker run -e "PKG_RESOURCES_CACHE_ZIP_MANIFESTS=1" -v `pwd`/container/trac:/trac -v `pwd`/container/repos:/repos tooringanalytics/trac-server trac-admin /trac $@
